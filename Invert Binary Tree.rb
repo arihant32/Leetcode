dcbe5063709a089608e892796c015414466c13ab
Arihant Jain <arihantjain099@gmail.com>
2020-06-01 14:58:32 +0530

    Invert Binary Tree

---
+++ Invert Binary Tree.rb
@@ -0,0 +1,53 @@
+=begin
+
+Invert a binary tree.
+
+Example:
+
+Input:
+
+     4
+   /   \
+  2     7
+ / \   / \
+1   3 6   9
+Output:
+
+     4
+   /   \
+  7     2
+ / \   / \
+9   6 3   1
+Trivia:
+This problem was inspired by this original tweet by Max Howell:
+
+Google: 90% of our engineers use the software you wrote (Homebrew), but you can’t invert a binary tree on a whiteboard so f*** off.
+
+=end
+
+# Definition for a binary tree node.
+# class TreeNode
+#     attr_accessor :val, :left, :right
+#     def initialize(val = 0, left = nil, right = nil)
+#         @val = val
+#         @left = left
+#         @right = right
+#     end
+# end
+# @param {TreeNode} root
+# @return {TreeNode}
+
+def invert_tree(root)
+    return root if root.nil?
+    
+    l = invert_tree root.left
+    r = invert_tree root.right
+    
+    root.left = r
+    root.right = l
+    
+    return root
+    
+end
+
+

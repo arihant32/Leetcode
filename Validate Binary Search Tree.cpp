5ac1260d45654af6f5317e086af5137b1ee6ce9a
Arihant Jain <arihantjain099@gmail.com>
2020-06-16 03:16:48 +0530

    Create Validate Binary Search Tree.cpp

---
+++ Validate Binary Search Tree.cpp
@@ -0,0 +1,67 @@
+/*
+
+Validate Binary Search Tree
+
+Given a binary tree, determine if it is a valid binary search tree (BST).
+
+Assume a BST is defined as follows:
+
+The left subtree of a node contains only nodes with keys less than the node's key.
+The right subtree of a node contains only nodes with keys greater than the node's key.
+Both the left and right subtrees must also be binary search trees.
+ 
+
+Example 1:
+
+    2
+   / \
+  1   3
+
+Input: [2,1,3]
+Output: true
+Example 2:
+
+    5
+   / \
+  1   4
+     / \
+    3   6
+
+Input: [5,1,4,null,null,3,6]
+Output: false
+Explanation: The root node's value is 5 but its right child's value is 4.
+
+
+*/
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    
+    bool isValid(TreeNode* root,long int min, long int max) {
+        if(root == NULL)
+            return true;
+        if(max < root->val || min > root->val)
+            return false;
+
+        long int l_range = (long)root->val - 1;
+        long int r_range = (long)root->val + 1;
+
+        return isValid(root->left, min, l_range) && isValid(root->right, r_range, max);
+    }
+    
+    bool isValidBST(TreeNode* root) {
+        return isValid(root, INT_MIN, INT_MAX);
+    }
+};

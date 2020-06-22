8e4c411c6833c8c770b707d86eddba017e9afd64
Arihant Jain <arihantjain099@gmail.com>
2020-06-22 23:22:37 +0530

    Update Tree : Top View.cpp

--- Tree : Top View.cpp
+++ Tree : Top View.cpp
@@ -65,42 +65,7 @@ From the top only nodes 1,2,5,6 will be visible.
 // Solution : Used level order traversal to solved it
 
 
-#include<bits/stdc++.h>
-
-using namespace std;
-
-class Node {
-    public:
-        int data;
-        Node *left;
-        Node *right;
-        Node(int d) {
-            data = d;
-            left = NULL;
-            right = NULL;
-        }
-};
-
-class Solution {
-    public:
-  		Node* insert(Node* root, int data) {
-            if(root == NULL) {
-                return new Node(data);
-            } else {
-                Node* cur;
-                if(data <= root->data) {
-                    cur = insert(root->left, data);
-                    root->left = cur;
-                } else {
-                    cur = insert(root->right, data);
-                    root->right = cur;
-               }
-
-               return root;
-           }
-        }
-
-/*
+#/*
 class Node {
     public:
         int data;

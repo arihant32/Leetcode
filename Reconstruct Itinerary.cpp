0a4a2183cb06d909f529dd8f972ae2b2c8f96e0c
Arihant Jain <arihantjain099@gmail.com>
2020-06-29 01:06:12 +0530

    Update Reconstruct Itinerary.cpp

--- Reconstruct Itinerary.cpp
+++ Reconstruct Itinerary.cpp
@@ -1,6 +1,6 @@
 /*
 
-Reconstruct Itinerary
+Reconstruct Itinerary or (eular path problem)
 
 Given a list of airline tickets represented by pairs of departure and arrival airports [from, to], reconstruct the itinerary in order. All of the tickets belong to a man who departs from JFK. Thus, the itinerary must begin with JFK.
 

91580d4387ec808d74cb6c4cbb2d13382e465532
Arihant Jain <arihantjain099@gmail.com>
2020-07-12 04:33:56 +0530

    Create Swap Nodes in Pairs.cpp

---
+++ Swap Nodes in Pairs.cpp
@@ -0,0 +1,50 @@
+/*
+
+Swap Nodes in Pairs
+
+
+Given a linked list, swap every two adjacent nodes and return its head.
+
+You may not modify the values in the list's nodes, only nodes itself may be changed.
+
+ 
+
+Example:
+
+Given 1->2->3->4, you should return the list as 2->1->4->3.
+
+*/
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* swap(ListNode* head) {
+        if(head == NULL || head->next == NULL) return head;
+        ListNode* current = head, *prev = NULL;
+        int cnt=0;
+        while(cnt < 2 && current != NULL) {
+            ListNode* temp = current->next;
+            current->next = prev;
+            prev = current;
+            current = temp;
+            cnt++;
+        }
+        if(current)
+            head->next = swap(current);
+        return prev;
+    }
+    
+    ListNode* swapPairs(ListNode* head) {
+        return swap(head);   
+    }
+};

cd3939fd80e306b3419d7d49f66c597d73bfe5f0
Arihant Jain <arihantjain099@gmail.com>
2020-09-05 22:26:52 +0530

    Update Kth Largest Element in an Array.cpp

--- Kth Largest Element in an Array.cpp
+++ Kth Largest Element in an Array.cpp
@@ -115,3 +115,26 @@ public:
         
     }
 };
+
+
+
+// or
+
+
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int, vector<int>, greater<int>> minheap;
+        
+        for(int i=0; i<nums.size(); i++) {
+            if(minheap.size() < k){
+                minheap.push(nums[i]);
+            }else{
+                minheap.push(nums[i]);
+                minheap.pop();
+            }
+        }
+        
+        return minheap.top();
+    }
+};
